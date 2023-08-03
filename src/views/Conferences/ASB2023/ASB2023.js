c7651614525f0275eb27558ea5ac151c06c792d9
ccarrasquillo3 <ccarrasquillo3@gatech.edu>
2023-08-03 13:47:04 -0400

    enabled full screen ASB 2023 video

--- src/views/Conferences/ASB2023/ASB2023.js
+++ src/views/Conferences/ASB2023/ASB2023.js
@@ -57,7 +57,7 @@ class HowItsMade extends React.Component {
                     <div className="two-col-format-column">
                         <div className="conference-col-right">
                             <div className="iframe-container">
-                                <iframe width="560" height="315" src="https://www.youtube.com/embed/Mh0tAw-QQfc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                <iframe width="560" height="315" src="https://www.youtube.com/embed/Mh0tAw-QQfc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
                             </div>
                         </div>
                     </div>
