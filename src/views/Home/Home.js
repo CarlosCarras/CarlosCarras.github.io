528235ec3ccef265ab4409f68600fa0b90cf4b56
ccarrasquillo3 <ccarrasquillo3@gatech.edu>
2024-07-31 10:37:29 -0400

    fixed uncontrolled terminal resizing :bug: attempt #2

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Typewriter from "typewriter-effect"
 import "./Home.css"
 
@@ -83,29 +83,26 @@ function Home(props) {
     const [isNarrow, setIsNarrow] = useState(false);
     const [terminalPos, setTerminalPos] = useState({x: Math.max((0.4*window.innerWidth-400)/2, 0), y: 50});
 
-    const handleResize = () => {
+    const handleResize = useCallback(() => {
         const _isNarrow = window.innerWidth < 950;
         const posMultiplier = _isNarrow ? 0.9 : 0.45;
         const widthMultiplier = _isNarrow ? 0.8 : 0.4;
         const heightMultiplier = 0.8;
-        
-        setTerminalPos({x: posMultiplier*window.innerWidth-terminalWidth/2, y: 50});
-        setTerminalWidth(Math.min(widthMultiplier*window.innerWidth, TERMINAL_MAXDIMS.w));
-        setTerminalHeight(Math.min(heightMultiplier*window.innerHeight, TERMINAL_MAXDIMS.h));
+
+        setTerminalPos({x: posMultiplier * window.innerWidth - terminalWidth / 2, y: 50});
+        setTerminalWidth(Math.min(widthMultiplier * window.innerWidth, TERMINAL_MAXDIMS.w));
+        setTerminalHeight(Math.min(heightMultiplier * window.innerHeight, TERMINAL_MAXDIMS.h));
         setIsNarrow(_isNarrow);
         setGearTrainScale(0.1 + 0.00025 * window.innerWidth);
-    };
+    }, [terminalWidth]);
 
     useEffect(() => {
         window.addEventListener("resize", handleResize);
-        
+        handleResize(); // Initial resize on component mount
         return () => {
             window.removeEventListener("resize", handleResize);
         };
-    }, [terminalWidth]);
-
-    /* resize on startup */
-    useEffect(() => handleResize());
+    }, [handleResize]);
 
     useEffect(() => {
         setDarkMode(props.darkmode)
